2b25bd2a2d40ba800c9f0fa73f967964dd04b72c
Paul Braun <paul.braun@mnhn.lu>
2023-02-01 11:54:03 +0100

    Update 1-Agents.sql

--- luxembourg/1-Agents.sql
+++ luxembourg/1-Agents.sql
@@ -1,30 +1,34 @@
 select 
-	I.NAME_KEY as agentID,
-	TRIM(concat(I.FORENAME , ' ', I.SURNAME )) as preferredAgentName,
+	I."NAME_KEY" as agentID,
+	TRIM(concat(I."FORENAME" , ' ', I."SURNAME" )) as preferredAgentName,
 	'PERSON' as agentType
-from INDIVIDUAL I
-	union 
+from foreign_all."INDIVIDUAL" I
+
+union 
+
 select 
-	O.NAME_KEY as agentID,
-	O.FULL_NAME as preferredAgentName,
+	O."NAME_KEY" as agentID,
+	O."FULL_NAME" as preferredAgentName,
 	'ORGANISATION' as agentType
-from ORGANISATION O
+from foreign_all."ORGANISATION" O
 
 
 select 
-	C.Item_Name as preferredAgentName,
-	C.Collection_Unit_Key as agentID,
+	C."Item_Name" as preferredAgentName,
+	C."Collection"_Unit_Key as agentID,
 	'COLLECTION' as agentType
-from Collection C
+from foreign_all."Collection" C
 
 select 
-	NR.NAME_KEY_1 as subjectAgentID,
-	NR.ROLE as relationshipTo,
-	NR.NAME_KEY_1 as objectAgentID
-from NAME_RELATION NR
+	NR."NAME_KEY_1" as subjectAgentID,
+	NR."ROLE" as relationshipTo,
+	NR."NAME_KEY_1" as objectAgentID
+from foreign_all."NAME_RELATION" NR
+
 	union 
+	
 select 
-	C.Parent_Collection_Collection_Unit_Key as subjectAgentID,
+	C."Parent_Collection_Collection_Unit_Key" as subjectAgentID,
 	'parent of' as relationshipTo,
-	C.Collection_Unit_Key as objectAgentID
-from Collection C
+	C."Collection_Unit_Key" as objectAgentID
+from foreign_all."Collection" C
