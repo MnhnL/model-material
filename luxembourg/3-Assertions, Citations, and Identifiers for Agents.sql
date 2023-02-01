04f50d9915964e9b48d550f37c29ef99ab84773e
Paul Braun <paul.braun@mnhn.lu>
2023-02-01 12:40:10 +0100

    Update 3-Assertions, Citations, and Identifiers for Agents.sql

--- luxembourg/3-Assertions, Citations, and Identifiers for Agents.sql
+++ luxembourg/3-Assertions, Citations, and Identifiers for Agents.sql
@@ -1,55 +1,54 @@
 
+##### gets IDs for people
 select 
-I.NAME_KEY as identifierTargetID,
+I."NAME_KEY" as identifierTargetID,
 'AGENT' as identifierTargetType,
 	CASE	
 		WHEN 
-			SF.FILE_NAME like  '%wikidata%' 
+			SF."FILE_NAME" like  '%wikidata%' 
 				THEN 'WIKIDATA'
 		WHEN 
-			SF.FILE_NAME like  '%ORCID%' 
+			SF."FILE_NAME" like  '%orcid%' 
 				THEN 'ORCID' 
 		END as identifierType,
-	SF.FILE_NAME as identifierValue
-
-from INDIVIDUAL I
-	join NAME_SOURCES NS on I.name_key =  NS.name_key
-	join SOURCE_FILE SF on NS.SOURCE_KEY = SF.SOURCE_KEY
-where FILE_NAME like '%wikidata%' or FILE_NAME like '%ORCID%'
-	UNION
+	SF."FILE_NAME" as identifierValue
+from foreign_all."INDIVIDUAL" I
+	join foreign_all."NAME_SOURCES" NS on I."NAME_KEY" =  NS."NAME_KEY"
+	join foreign_all."SOURCE_FILE" SF on NS."SOURCE_KEY" = SF."SOURCE_KEY"
+where SF."FILE_NAME" like '%wikidata%' or SF."FILE_NAME" like '%ORCID%'
+	union 
+##### gets IDs for organisations
 select 
-O.NAME_KEY as identifierTargetID,
+O."NAME_KEY" as identifierTargetID,
 'AGENT' as identifierTargetType,
 	CASE	
 		WHEN 
-			SF.FILE_NAME like  '%wikidata%' 
+			SF."FILE_NAME" like  '%wikidata%' 
 				THEN 'WIKIDATA'
 		WHEN 
-			SF.FILE_NAME like  '%ROR%' 
+			SF."FILE_NAME" like  '%ror%' 
 				THEN 'ROR' 
 		END as identifierType,
-	SF.FILE_NAME as identifierValue
-
-from ORGANISATION O
-	join NAME_SOURCES NS on O.name_key =  NS.name_key
-	join SOURCE_FILE SF on NS.SOURCE_KEY = SF.SOURCE_KEY
-where FILE_NAME like '%wikidata%' or FILE_NAME like '%ROR%'
+	SF."FILE_NAME" as identifierValue
+from foreign_all."ORGANISATION" O
+	join foreign_all."NAME_SOURCES" NS on O."NAME_KEY" =  NS."NAME_KEY"
+	join foreign_all."SOURCE_FILE" SF on NS."SOURCE_KEY" = SF."SOURCE_KEY"
+where "FILE_NAME" like '%wikidata%' or "FILE_NAME" like '%ROR%'
 UNION
+##### gets IDs for collections
 select 
-C.Collection_Unit_Key as identifierTargetID,
+C."Collection_Unit_Key" as identifierTargetID,
 'COLLECTION' as identifierTargetType,
 	CASE	
 		WHEN 
-			SF.FILE_NAME like  '%wikidata%' 
+			SF."FILE_NAME" like  '%wikidata%' 
 				THEN 'WIKIDATA'
 		WHEN 
-			SF.FILE_NAME like  '%grscicoll%' 
+			SF."FILE_NAME" like  '%grscicoll%' 
 				THEN 'GRSCICOLL' 
 		END as identifierType,
-	SF.FILE_NAME as identifierValue
-
-from Collection C
-	 join Source_Join SJ on C.Collection_Unit_Key =  SJ.Record_Key
-	 join SOURCE_FILE SF on SJ.SOURCE_KEY = SF.SOURCE_KEY
-where SF.FILE_NAME like '%wikidata%' or SF.FILE_NAME like '%gbif%'
-
+	SF."FILE_NAME" as identifierValue
+from foreign_all."Collection" C
+	 join foreign_all."Source_Join" SJ on C."Collection_Unit_Key" =  SJ."Record_Key"
+	 join foreign_all."SOURCE_FILE" SF on SJ."Record_Key" = SF."SOURCE_KEY"
+where SF."FILE_NAME" like '%wikidata%' or SF."FILE_NAME" like '%gbif%'
