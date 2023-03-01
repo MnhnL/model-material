b2da5a579cdf6e3ede93c01fa4ffd2b8fe63daee
Paul Braun <paul.braun@mnhn.lu>
2023-03-01 14:19:38 +0100

    Update taxon_identification.sql

--- luxembourg/taxon_identification.sql
+++ luxembourg/taxon_identification.sql
@@ -1,1 +1,9 @@
-
+select 
+	D."Concept_Key" as taxon_id ,
+	D."Determination_Key" as identification_id ,
+	'' as taxon_order , --  not sure about this, numerique seuqence?
+	'' as taxon_authority , -- not sure what this is
+	'' as taxon_confidence_percent  -- not something used by MNHNL
+  --? PRIMARY KEY (taxon_id, identification_id, taxon_order)
+	
+from foreign_all."Determination" D 
